bedc69696d231096833b50a72f33d3cf34fbe792
saeed <s8990@yahoo.com>
2021-11-19 00:57:12 +0330

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/s8990/go-bookStore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
+}
